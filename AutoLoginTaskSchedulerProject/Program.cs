1963088db93f27db6e6dfd3a2afc5163901b03d7
Marco <m4rkoup@gmail.com>
2020-10-27 12:25:23 -0400

    Add source code for auto start as admin solution

---
+++ AutoLoginTaskSchedulerProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Win32.TaskScheduler;
+
+// This project is the one in charge 
+// of the auto start mechanism usen by 
+// Openstream server to start automatically
+// When an user logs on.
+// The scheduled task can run with All the 
+// admin privileges.
+
+namespace ScheduleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("[ScheduleTask] No argument received for task schedule. Error.");
+                return;
+            }
+
+            String executablePath = args[0];
+            String executableDir = args[1];
+
+            System.Console.WriteLine($"[ScheduleTask] Path:{executablePath}\n[ScheduleTask] Dir:{executableDir}");
+
+            // Create a new task definition for the local machine and assign properties
+            TaskDefinition td = TaskService.Instance.NewTask();
+            td.RegistrationInfo.Description = "Open Stream Server Task";
+
+            //Allows the task to run with admin rights
+            td.Principal.RunLevel = TaskRunLevel.Highest;
+
+            //Allows the task to run infinetly. 
+            td.Settings.ExecutionTimeLimit.Add(TimeSpan.Zero);
+
+            LogonTrigger lt = new LogonTrigger();
+            lt.Delay = TimeSpan.FromMinutes(1); // V2 only
+
+
+            td.Triggers.Add(lt);
+            // Create an action that will launch Notepad whenever the trigger fires
+            td.Actions.Add(executablePath, null, executableDir);
+            // Register the task in the root folder of the local machine
+            TaskService.Instance.RootFolder.RegisterTaskDefinition("OpenStream autologon start", td);
+        }
+    }
+}
---
+++ headers/auto_start_task_schedule.h
@@ -0,0 +1,10 @@
+#ifndef AUTO_START_TASK_SCHEDULE_H
+#define AUTO_START_TASK_SCHEDULE_H
+
+/**
+  Defines the programatically adition of application auto start
+  after login on windows 10, with the highest privileges.
+  **/
+int create_task_schedule_auto_start();
+
+#endif // AUTO_START_TASK_SCHEDULE_H
